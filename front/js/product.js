b7ae6927ac1bba6657b35f14af1d994d898c25ce
SonimJunior <sonim7@hotmail.fr>
2021-12-07 02:41:22 +0100

    commit

--- front/js/product.js
+++ front/js/product.js
@@ -3,97 +3,97 @@ const dataID = new URLSearchParams(location.search).get("_id");
 
 //Fonction pour afficher les elements de l'API 
 function getDataFromApi() {
-  fetch(`http://localhost:3000/api/products/${dataID}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      function dataProduct() {
-        //On selectionne la balise HTML pour intégrer l'image
-        const image = document.querySelector(".item__img");
-        image.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+    fetch(`http://localhost:3000/api/products/${dataID}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            function dataProduct() {
+                //On selectionne la balise HTML pour intégrer l'image
+                const image = document.querySelector(".item__img");
+                image.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
 
-        //affichage du prix du produit
-        const price = document.getElementById("price");
-        price.innerText = `${data.price}`;
+                //affichage du prix du produit
+                const price = document.getElementById("price");
+                price.innerText = `${data.price}`;
 
-        //affichage du nom du produit
-        const name = document.getElementById("title");
-        name.innerHTML = `<h1>${data.name}</h1>`;
+                //affichage du nom du produit
+                const name = document.getElementById("title");
+                name.innerHTML = `<h1>${data.name}</h1>`;
 
-        //affichage de la description du produit
-        const description = document.getElementById("description");
-        description.innerText = `${data.description}`;
+                //affichage de la description du produit
+                const description = document.getElementById("description");
+                description.innerText = `${data.description}`;
 
-        //affichage des differentes couleurs du produit
-        for (index = 0; index < data.colors.length; index++) {
-          document.getElementById("colors").innerHTML += `<option value="${data.colors[index]}">${data.colors[index]}</option>`;
-        }
-      }
-      dataProduct();
-
-      // Ajouter élements dans localstorage 
-      // Ajouter au panier 
-      function setLocalStorage() {
-        document
-          .getElementById("addToCart")
-          .addEventListener("click", (event) => {
-            event.preventDefault();
-            let dataQuantity = document.getElementById("quantity");
-            const dataColor = document.getElementById("colors");
-            // Conditition : la quantité est > 0 et qu'une couleur est selectionné, alors le produit est envoyé au panier
-            if (100>=dataQuantity.value && dataQuantity.value > 0 && dataColor.value !== "") {
-              const storageArray = {
-                id: dataID,
-                name: data.name,
-                price: data.price,
-                color: dataColor.value,
-                quantity: dataQuantity.value,
-                image: data.imageUrl,
-                alt: data.altTxt,
-              };
-              // Produit envoyé dans le localstorage en format JSON
-              let localStorageProducts = JSON.parse(
-                localStorage.getItem("localStorageProducts")
-              );
-              // Condition : LocalStorage vide
-              if (localStorageProducts === null) {
-                localStorageProducts = [];
-              }
-              let productAdded = false;
-
-              // Si le produit ajouté est un article déjà dans le panier, seulement 1 article sera ajouté.
-              localStorageProducts.forEach((product) => {
-                if (product.id === dataID && product.color === dataColor.value) {
-                  1>=dataQuantity.value && dataColor.value !== 
-                  product.quantity++
-                  productAdded = true;
-                  if (dataQuantity.value > 1) {
-                    alert ("Merci d'ajouter seulement 1 article à la fois.")
-                  }
+                //affichage des differentes couleurs du produit
+                for (index = 0; index < data.colors.length; index++) {
+                    document.getElementById("colors").innerHTML += `<option value="${data.colors[index]}">${data.colors[index]}</option>`;
                 }
-              });
-              
-              // Si le produit ajouté est un nouvel article 
-              if (!productAdded) {
-                // ajoute l'élement au tableau et retourne la nouvelle taille du tableau
-                localStorageProducts.push(storageArray);
-              } // ajoute dans l'emplacement de stockage
-              localStorage.setItem(
-                "localStorageProducts",
-                JSON.stringify(localStorageProducts)
-              ); 
-            
-            } else {
-              alert("Attention, le nombre d'article doit être compris entre 1 et 100 maximum."); 
             }
-          });
-      }
-      setLocalStorage();
-    })
-    // Si l'API ne répond pas, un message d'erreur apparait 
-    .catch((error) => {
-      alert("Le serveur ne répond pas pour le moment.")
-    });
+            dataProduct();
+
+            // Ajouter élements dans localstorage 
+            // Ajouter au panier 
+            function setLocalStorage() {
+                document
+                    .getElementById("addToCart")
+                    .addEventListener("click", (event) => {
+                        event.preventDefault();
+                        let dataQuantity = document.getElementById("quantity");
+                        const dataColor = document.getElementById("colors");
+                        // Conditition : la quantité est > 0 et qu'une couleur est selectionné, alors le produit est envoyé au panier
+                        if (100 >= dataQuantity.value && dataQuantity.value > 0 && dataColor.value !== "") {
+                            const storageArray = {
+                                id: dataID,
+                                name: data.name,
+                                price: data.price,
+                                color: dataColor.value,
+                                quantity: dataQuantity.value,
+                                image: data.imageUrl,
+                                alt: data.altTxt,
+                            };
+                            // Produit envoyé dans le localstorage en format JSON
+                            let localStorageProducts = JSON.parse(
+                                localStorage.getItem("localStorageProducts")
+                            );
+                            // Condition : LocalStorage vide
+                            if (localStorageProducts === null) {
+                                localStorageProducts = [];
+                            }
+                            let productAdded = false;
+
+                            // Si le produit ajouté est un article déjà dans le panier, seulement 1 article sera ajouté.
+                            localStorageProducts.forEach((product) => {
+                                if (product.id === dataID && product.color === dataColor.value) {
+                                    1 >= dataQuantity.value && dataColor.value !==
+                                        product.quantity++
+                                    productAdded = true;
+                                    if (dataQuantity.value > 1) {
+                                        alert("Merci d'ajouter seulement 1 article à la fois.")
+                                    }
+                                }
+                            });
+
+                            // Si le produit ajouté est un nouvel article 
+                            if (!productAdded) {
+                                // ajoute l'élement au tableau et retourne la nouvelle taille du tableau
+                                localStorageProducts.push(storageArray);
+                            } // ajoute dans l'emplacement de stockage
+                            localStorage.setItem(
+                                "localStorageProducts",
+                                JSON.stringify(localStorageProducts)
+                            );
+
+                        } else {
+                            alert("Attention, le nombre d'article doit être compris entre 1 et 100 maximum.");
+                        }
+                    });
+            }
+            setLocalStorage();
+        })
+        // Si l'API ne répond pas, un message d'erreur apparait 
+        .catch((error) => {
+            alert("Le serveur ne répond pas pour le moment.")
+        });
 }
 getDataFromApi();
--- front/js/script.js
+++ front/js/script.js
@@ -1,15 +1,15 @@
 //Appel de l'API
 function apiCall() {
   fetch("http://localhost:3000/api/products")
-    // Transformation des données en format json
-    .then((response) => response.json())
-    .then((data) => {
-      displayProducts(data);
-    })
-    // Si l'API ne répond pas, un message d'erreur apparait
-    .catch((error) => {
-      alert("Le serveur ne répond pas pour le moment.")
-    });
+      // Transformation des données en format json
+      .then((response) => response.json())
+      .then((data) => {
+          displayProducts(data);
+      })
+      // Si l'API ne répond pas, un message d'erreur apparait
+      .catch((error) => {
+          alert("Le serveur ne répond pas pour le moment.")
+      });
 }
 apiCall();
 
@@ -17,18 +17,18 @@ apiCall();
 function displayProducts(data) {
   //for...of permet de créer une boucle Array qui parcourt un objet itérable
   for (const product of data) {
-    // Insère les noeuds du DOM à une position spécifique
-    const cardProducts = `
-          <a href="./product.html?_id=${product._id}">
-          <article>
-              <img src="${product.imageUrl}" alt="${product.altTxt}">
-             <h3 class="productName">${product.name}</h3>
-             <p class="productDescription">${product.description}</p>
-          </article>
-          </a>
-        `;
-    document
-      .getElementById("items")
-      .insertAdjacentHTML("beforeend", cardProducts);
+      // Insère les noeuds du DOM à une position spécifique
+      const cardProducts = `
+        <a href="./product.html?_id=${product._id}">
+        <article>
+            <img src="${product.imageUrl}" alt="${product.altTxt}">
+           <h3 class="productName">${product.name}</h3>
+           <p class="productDescription">${product.description}</p>
+        </article>
+        </a>
+      `;
+      document
+          .getElementById("items")
+          .insertAdjacentHTML("beforeend", cardProducts);
   }
 }
